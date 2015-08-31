3a1a5d05458c2918b7dece7d85e021691d498ce9
Marshall Davis <toothlessrebel@gmail.com>
2015-08-30 21:31:58 -0400

    Some loot tracking.

--- lootTracker/static/lootTracker/js/lootTracker.js
+++ lootTracker/static/lootTracker/js/lootTracker.js
@@ -3,5 +3,34 @@
  */
 
 $(function () {
-    $('.dropdown').dropdown();
+    var item_not_found = '';
+    var item_dropdown = $('.item.dropdown');
+    var lookup_fired = false;
+
+    item_dropdown.dropdown({
+        allowAdditions: true,
+        onNoResults: function (searched_for) {
+            item_not_found = searched_for;
+        }
+    });
+
+    item_dropdown.on('change', function () {
+        if (item_not_found.length > 0 && !lookup_fired) {
+            // Hit up Fuzzworks to get the ID.
+            lookup_fired = true;
+            $.ajax({
+                url: 'https://www.fuzzwork.co.uk/api/typeid.php',
+                data: {
+                    typename: item_not_found
+                }
+            }).always(function () {
+                lookup_fired = false;
+            }).success(function (response) {
+                console.log(response);
+                
+            }).error(function () {
+                // @todo
+            });
+        }
+    });
 });
